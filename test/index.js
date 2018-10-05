e085733a65302eae49b2162de2e4210ca8bbba22
wswoodruff <williamswoodruff@gmail.com>
2018-10-04 22:15:47 -0400

    Start fleshing out api funcs to work with the teset suite

---
+++ test/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Lab = require('lab');
+const Code = require('code');
+
+const DoggoAdapterGpg = require('../lib')({});
+const Doggo = require('doggo')(DoggoAdapterGpg);
+
+const DoggoAdapterTestSuite = require('doggo/test/adapterTestSuite');
+
+// Test shortcuts
+
+const labScript = exports.lab = Lab.script();
+const { it, describe, before, after } = labScript;
+const expect = Code.expect;
+
+describe('doggo-adapter-gpg', () => {
+
+    it('passes with the gpg adapter', async (flags) => {
+
+        const testSuite = new DoggoAdapterTestSuite(DoggoAdapterGpg, {
+            expect, lab: { it, describe, before, after }
+        });
+
+        testSuite.genTests();
+    });
+});
