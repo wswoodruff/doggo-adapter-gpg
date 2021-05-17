a9e45246c59bef1c788c740cd51811b9c30470ce
wswoodruff <williamswoodruff@gmail.com>
2021-05-17 18:03:09 -0400

    Get a mock suite passing

--- test/index.js
+++ test/index.js
@@ -3,28 +3,27 @@
 const Code = require('@hapi/code');
 const Lab = require('@hapi/lab');
 
-const DoggoAdapterGpg = require('../lib');
+const DoggoGpg = require('../lib');
 const DoggoAdapterTestSuite = require('@xdcreative/doggo/test/adapter-test-suite');
 
 // Test shortcuts
 
 const labScript = exports.lab = Lab.script();
-// const { it, describe, before, after } = labScript;
 const { it, describe } = labScript;
 const expect = Code.expect;
 
-describe('doggo-adapter-gpg', () => {
+const TEST_UTILS = {
+    expect,
+    describe,
+    it
+};
 
-    it('passes the doggo adapter-test-suite', (flags) => {
+describe('doggo-adapter-gpg', () => {});
 
-        const testSuite = new DoggoAdapterTestSuite(DoggoAdapterGpg, {
-            expect,
-            describe,
-            it
-        });
+/*
+    =========================================
+    Run DoggoAdapterTestSuite on doggo-gpg
+    =========================================
+*/
 
-        console.log('testSuite', testSuite);
-
-        // testSuite.run();
-    });
-});
+new DoggoAdapterTestSuite(DoggoGpg, TEST_UTILS).run();
