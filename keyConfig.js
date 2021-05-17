38d38b06e65dc8d56fe1fb948e9cf0ea7ed6e948
wswoodruff <williamswoodruff@gmail.com>
2021-05-17 17:53:54 -0400

    Pass linter

--- keyConfig.js
+++ keyConfig.js
@@ -2,30 +2,32 @@
 
 const Os = require('os');
 
-const Dedent = require('../lib/vendor/dedent');
+const Dedent = require('./vendor/dedent/dist/dedent');
 
 const internals = {};
 
 // We <3 ed25519/cv25519
-const PRIMARY_KEY = 'ed25519';
+const KEY_TYPE = 'eddsa';
+const KEY_CURVE = 'ed25519';
 const SUBKEY_TYPE = 'ecdh';
 const SUB_KEY_CURVE = 'cv25519';
 
 module.exports = {
-    get: (name, password, comment, email) => {
+    get: ({ name, password, comment, email }) => {
 
         const {
             lineIfParam,
             removeEmptyLines
         } = internals;
 
-        return removeEmptyLines(Dedent`Key-Type: eddsa
-            Key-Curve: ${PRIMARY_KEY}
+        return removeEmptyLines(Dedent`
+            Key-Type: ${KEY_TYPE}
+            Key-Curve: ${KEY_CURVE}
             Key-Usage: sign
             Subkey-Type: ${SUBKEY_TYPE}
             Subkey-Curve: ${SUB_KEY_CURVE}
             Subkey-Usage: encrypt
-            Passphrase: ${password || 'test'}
+            Passphrase: ${password}
             Expire-Date: 0
             ${lineIfParam(name, `Name-Real: ${name}`)}
             ${lineIfParam(comment, `Name-Comment: Doggo user - ' + ${comment}`)}
--- test/index.js
+++ test/index.js
@@ -9,18 +9,21 @@ const DoggoAdapterTestSuite = require('@xdcreative/doggo/test/adapter-test-suite
 // Test shortcuts
 
 const labScript = exports.lab = Lab.script();
-const { it, describe, before, after } = labScript;
+// const { it, describe, before, after } = labScript;
+const { it, describe } = labScript;
 const expect = Code.expect;
 
 describe('doggo-adapter-gpg', () => {
 
-    it('passes the doggo adapter-test-suite', async (flags) => {
+    it('passes the doggo adapter-test-suite', (flags) => {
 
         const testSuite = new DoggoAdapterTestSuite(DoggoAdapterGpg, {
             expect,
             describe,
             it
         });
+
+        console.log('testSuite', testSuite);
 
         // testSuite.run();
     });
